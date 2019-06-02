4793d4b3b64690a1d4bd5a1abd78c815beaf1580
WAVIFICATION <jerin.jacob1@gmail.com>
2019-06-02 18:11:38 +1000

    optimised

--- project_IFN509/project.r
+++ project_IFN509/project.r
@@ -1,13 +1,15 @@
 library(ggplot2)
-southBrisTotal<- read.csv("/Users/nikhilnair/GitHub/QUT/project_IFN509/southbrisbane-aq-2018.csv")
-weatherTotal<- read.csv("/Users/nikhilnair/GitHub/QUT/project_IFN509/weatherAUS.csv")
+library(corrplot)
+
+southBrisTotal<- read.csv("southbrisbane-aq-2018.csv")
+weatherTotal<- read.csv("weatherAUS.csv")
 #class(southBrisTotal$Time)
 southBris<-subset(southBrisTotal,!is.na(southBrisTotal$Date))
 southBris$Date<-as.Date(southBris$Date, format="%d/%m/%Y")
 weatherTotal$Date<-as.Date(weatherTotal$Date, format="%Y-%m-%d")
 
 southBris9am<-subset(southBris,southBris$Time=="09:00")
-summary(southBris9am)
+#summary(southBris9am)
 southBris3pm<-subset(southBris,southBris$Time=="15:00")
 #names(southBris9am)
 predictablesAirQualityNames<-c("Relative.Humidity....", "Nitrogen.Oxide..ppm.", "Nitrogen.Dioxide..ppm.", "Nitrogen.Oxides..ppm.", "Carbon.Monoxide..ppm.", "PM10..ug.m.3.", "PM2.5..ug.m.3.")
@@ -65,7 +67,7 @@ unpredictableairqualities<-c("Wind.Direction..degTN.","Wind.Speed..m.s.","Wind.S
 for (i in (unpredictableairqualities))
 {
   
-  print(complete.cases(southBris9am[[i]]))
+  #print(complete.cases(southBris9am[[i]]))
   southBris9am <- southBris9am[complete.cases(southBris9am[[i]]),]
 }
 #
@@ -75,8 +77,8 @@ unpredictableairqualities<-c("Wind.Direction..degTN.","Wind.Speed..m.s.","Wind.S
 for (i in (unpredictableairqualities))
 {
   
-  print(complete.cases(southbris3pm[[i]]))
-  southbris3pm <- southbris3pm[complete.cases(southbris3pm[[i]]),]
+  #print(complete.cases(southbris3pm[[i]]))
+  southBris3pm <- southBris3pm[complete.cases(southBris3pm[[i]]),]
 }
 #
 
@@ -109,7 +111,7 @@ for (i in (predictableairqualities))
         ani<-ani+1
       }
       southBris9am[[i]][j]<-mean(temp)
-      print(southBris9am[[i]][j])
+      #print(southBris9am[[i]][j])
     }
   }
 }
@@ -141,7 +143,7 @@ for (i in (predictableairqualities))
         ani<-ani+1
       }
       southBris3pm[[i]][j]<-mean(temp)
-      print(southBris3pm[[i]][j])
+      #print(southBris3pm[[i]][j])
     }
   }
 }
@@ -175,7 +177,7 @@ for (a in (predictableairqualities))
         k<-k+1
       }
       southBris3pm[[i]][j]<-mean(temp)
-      print(southBris3pm[[i]][j])
+      #print(southBris3pm[[i]][j])
     }
   }
 }
@@ -207,11 +209,11 @@ for (a in (predictableairqualities))
         k<-k+1
       }
       southBris9am[[i]][j]<-mean(temp)
-      print(southBris9am[[i]][j])
+      #print(southBris9am[[i]][j])
     }
   }
 }
-summary(southBris9am)
+#summary(southBris9am)
 #
 #weather9am
 #Tissa
@@ -241,7 +243,7 @@ for (a in (predictableweatherqualities))
         k<-k+1
       }
       weather9am[[i]][j]<-mean(temp)
-      print(weather9am[[i]][j])
+      #print(weather9am[[i]][j])
     }
   }
 }
@@ -275,7 +277,7 @@ for (a in (predictableweatherqualities))
         k<-k+1
       }
       weather3pm[[i]][j]<-mean(temp)
-      print(weather3pm[[i]][j])
+      #print(weather3pm[[i]][j])
     }
   }
 }
@@ -292,7 +294,36 @@ names(total3pm)<-c("Date","MinTemp","MaxTemp","Rainfall","Evaporation","Sunshine
 names(total9am)<-c("Date","MinTemp","MaxTemp","Rainfall","Evaporation","Sunshine","WindGustDir","WindGustSpeed","RainToday","RISK_MM","RainTomorrow","WindDir","Humidity","Pressure","Cloud","Temp","Time","Wind.Direction..degTN.","Wind.Speed..m.s.","Wind.Sigma.Theta..deg.","Wind.Speed.Std.Dev..m.s.","Air.Temperature..degC.","Relative.Humidity....","Nitrogen.Oxide..ppm.","Nitrogen.Dioxide..ppm.","Nitrogen.Oxides..ppm.","Carbon.Monoxide..ppm.","PM10..ug.m.3.","PM2.5..ug.m.3.")
 total9am$Time<-"9am"
 total3pm$Time<-"3pm"
-totalData<-rbind(total9am,total3pm)
+totalData<-rbind(total9am,total3pm)#totaldata named as 9am and 3pm time
+
+#row.names(mydata)<-paste(mydata$Date,mydata$Time)
+t1<-total9am[, !(names(total9am) %in% c("Time","WindGustDir"))]
+t2<-total3pm[, !(names(total3pm) %in% c("Time","WindGustDir"))]
+
+for(xc in c("RainTomorrow","RainToday","WindDir")){#changing character value into enumerated ones
+  levels(t1[[xc]])<- 1:nlevels(t1[[xc]])
+  levels(t2[[xc]])<- 1:nlevels(t2[[xc]])
+  # print(xc)
+  # print(nlevels(t2[[xc]]))
+}
+for(xc in names(t1)){
+  t1[[xc]]<-as.numeric(t1[[xc]])
+  t2[[xc]]<-as.numeric(t2[[xc]])
+}
+
+# str(t1)
+AverageTimeTotalData<-t1[t1$Date %in% t2$Date,]
+for(i in 1:nrow(AverageTimeTotalData))#take avarage of 9am and 3pm data for each date: AverageTimeTotalData
+{
+  for (j in 1:nrow(t2)) {
+    if(t2$Date[j]==AverageTimeTotalData$Date[i]){
+      for(k in names(AverageTimeTotalData)){
+        AverageTimeTotalData[[k]][i]<-((AverageTimeTotalData[[k]][i]+t2[[k]][j])/2)
+      }
+      break()
+    }
+  }
+}
 
 #end_Integrate_Jerin
 #
@@ -310,49 +341,33 @@ corrplot(cor(as.matrix(cor_data)),method = "circle")
 #
 #
 #
-#start_DTree_Vineela_Jerin
-str(totalData)
-
-summary(totalData)
+#start_DTree_Vineela
 
 
-a <- filter(totalData, Nitrogen.Oxides..ppm.>= 0.0000 & Nitrogen.Oxides..ppm.<= 0.0130) %>%
+treedata <- filter(AverageTimeTotalData, Nitrogen.Oxides..ppm.>= 0.0000 & Nitrogen.Oxides..ppm.<= 0.0130) %>%
   mutate(Oxide_Range= "verylow")
 
+treedata <- rbind(treedata, filter(AverageTimeTotalData, Nitrogen.Oxides..ppm.> 0.0130 & Nitrogen.Oxides..ppm.<= 0.0230) %>%
+  mutate(Oxide_Range= "low"))
 
-b <- filter(totalData, Nitrogen.Oxides..ppm.> 0.0130 & Nitrogen.Oxides..ppm.<= 0.0230) %>%
-  mutate(Oxide_Range= "low")
+treedata <- rbind(treedata, filter(AverageTimeTotalData, Nitrogen.Oxides..ppm.> 0.0230 & Nitrogen.Oxides..ppm.<= 0.0302) %>%
+  mutate(Oxide_Range= "medium"))
 
+treedata <- rbind(treedata, filter(AverageTimeTotalData, Nitrogen.Oxides..ppm.> 0.0302 & Nitrogen.Oxides..ppm.<= 0.0390) %>%
+  mutate(Oxide_Range= "high"))
 
+treedata <- rbind(treedata, filter(AverageTimeTotalData, Nitrogen.Oxides..ppm.> 0.0390 & Nitrogen.Oxides..ppm.<= 0.2430) %>%
+  mutate(Oxide_Range= "veryhigh"))
 
-c <- filter(totalData, Nitrogen.Oxides..ppm.> 0.0230 & Nitrogen.Oxides..ppm.<= 0.0302) %>%
-  mutate(Oxide_Range= "medium")
-
-
-d <- filter(totalData, Nitrogen.Oxides..ppm.> 0.0302 & Nitrogen.Oxides..ppm.<= 0.0390) %>%
-  mutate(Oxide_Range= "high")
-
-
-e <- filter(totalData, Nitrogen.Oxides..ppm.> 0.0390 & Nitrogen.Oxides..ppm.<= 0.2430) %>%
-  mutate(Oxide_Range= "veryhigh")
-
-
-
-
-
-f <- rbind(a,b)
-g<- rbind(c,d)    
-h <- rbind(f,g)
-i <- rbind(h,e)
 
 
 fit <- rpart(Oxide_Range ~ Air.Temperature..degC. + Wind.Speed..m.s. +
                Wind.Direction..degTN. + Relative.Humidity....,
-             method="class", data=i)
+             method="class", data=treedata)
 
-printcp(fit) # display the results
-plotcp(fit) # visualize cross-validation results
-summary(fit) # detailed summary of splits
+# printcp(fit) # display the results
+# plotcp(fit) # visualize cross-validation results
+# summary(fit) # detailed summary of splits
 
 # plot tree
 plot(fit, uniform=TRUE,
@@ -371,33 +386,34 @@ text(pfit, use.n=TRUE, all=TRUE, cex=.8)
 
 
 #test and training data
-set.seed(1234)
-ind <- sample(2, nrow(i), replace=TRUE, prob=c(0.7, 0.3))
-ind
-train_data1 <- i[ind==1,]
-train_data1
-test_data1 <- i[ind==2,]
-test_data1
+# set.seed(1234)
+# ind <- sample(2, nrow(i), replace=TRUE, prob=c(0.7, 0.3))
+# ind
+# train_data1 <- i[ind==1,]
+# train_data1
+# test_data1 <- i[ind==2,]
+# test_data1
 
 #ctree
-train_data1$Wind.Direction..degTN.<-as.numeric(train_data1$Wind.Direction..degTN.)
-i$Oxide_Range<-as.factor(i$Oxide_Range)
-formula1 <-Oxide_Range ~ Air.Temperature..degC. + Wind.Speed..m.s. +
-  Wind.Direction..degTN. + Relative.Humidity.... 
-ctree1 <- ctree(formula1, data = train_data1)
-ctree1
 
-plot(ctree1)
-predictions <- predict(ctree1, newdata = test_data1)
+# train_data1$Wind.Direction..degTN.<-as.numeric(train_data1$Wind.Direction..degTN.)
+# i$Oxide_Range<-as.factor(i$Oxide_Range)
+# formula1 <-Oxide_Range ~ Air.Temperature..degC. + Wind.Speed..m.s. +
+#   Wind.Direction..degTN. + Relative.Humidity.... 
+# ctree1 <- ctree(formula1, data = train_data1)
+# ctree1
+# 
+# plot(ctree1)
+# predictions <- predict(ctree1, newdata = test_data1)
+# 
+# table(predictions, test_data1$Oxide_Range)
+# 
+# 
+# library(caret)
+# library(e1071)
+# confusionMatrix(predict(ctree1, newdata = test_data1), test_data1$Oxide_Range)
 
-table(predictions, test_data1$Oxide_Range)
-
-
-library(caret)
-library(e1071)
-confusionMatrix(predict(ctree1, newdata = test_data1), test_data1$Oxide_Range)
-
-#end_DTree_Jerin_Vineela
+#end_DTree_Vineela
 #
 #
 
@@ -406,41 +422,25 @@ confusionMatrix(predict(ctree1, newdata = test_data1), test_data1$Oxide_Range)
 #
 #
 #start_Clustering_Jerin
-#row.names(mydata)<-paste(mydata$Date,mydata$Time)
-total9am<-total9am[, !(names(total9am) %in% c("Time","WindGustDir"))]
-total3pm<-total3pm[, !(names(total3pm) %in% c("Time","WindGustDir"))]
-#row.names(total9am)<-total9am$Date
-#row.names(total3pm)<-total3pm$Date
-mydata<-total9am[total9am$Date %in% total3pm$Date,]
-for(i in 1:nrow(mydata))#take avarage of 9am and 3pm data for each date
-{
-  for (j in 1:nrows(total3pm)) {
-    if(total3pm$Date[j]==mydata$Date[i]){
-      for(k in names(mydata)){
-        mydata[[k]][i]<-(mydata[[k]][i]+total3pm[[k]][j])
-      }
-    }
-  }
-}
+mydata<-AverageTimeTotalData
+mydata$Date<-as.Date(mydata$Date)
 row.names(mydata)<-mydata$Date#naming rows with dates
-mydata<-mydata[, !(names(mydata) %in% c("Date"))]#removing dates from attrbute list
+mydata<-mydata[, !(names(mydata) %in% c("Date","Temp","WindDir","Wind.Sigma.Theta..deg.",
+                                        "Wind.Speed.Std.Dev..m.s.","Nitrogen.Oxide..ppm.",
+                                        "Nitrogen.Dioxide..ppm.","Cloud","RainTomorrow"))]#removing dates from attrbute list
 
 
-for( i in names(mydata)){
-  mydata[[i]]<-as.numeric(mydata[[i]])
-}
 
-mydata <- na.omit(mydata) # listwise deletion of missing
-mydata <- scale(mydata) # standardize variables
+
+ mydata <- scale(mydata) # standardize variables
 wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
-#fviz_nbclust(mydata, kmeans, method = "wss") +
-#  geom_vline(xintercept = 3, linetype = 2)+
-#  labs(subtitle = "Elbow method")
 set.seed(123)
-#fviz_nbclust(mydata, kmeans, nstart = 25,  method = "gap_stat", nboot = 100)+
-# labs(subtitle = "Gap statistic method")
-cluster<-kmeans(mydata, centers = 9, nstart = 25)
-fviz_cluster(cluster, data = mydata)
+fviz_nbclust(mydata, kmeans, nstart = 25,  method = "gap_stat", nboot = 100)+
+ labs(subtitle = "Gap statistic method")#Using gap method to identify best number of clusters
+
+
+cluster<-kmeans(mydata, centers = 3, nstart = 25)
+fviz_cluster(cluster, data = mydata)#clustering
 #end_Clustering_Jerin
 #
 #
